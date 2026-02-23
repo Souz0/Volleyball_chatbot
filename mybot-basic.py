515c74e67b42ef88defcb720ecb2bb0fea57a233
souzo <denisjordanov172@gmail.com>
2026-02-23 22:24:49 +0000

    fivbvis implementation alongside some additional questions

--- mybot-basic.py
+++ mybot-basic.py
@@ -2,7 +2,7 @@
 import time
 if not hasattr(time, 'clock'):
     time.clock = time.perf_counter
-
+import xml.etree.ElementTree as ET
 import aiml
 import wikipedia
 import fivbvis
@@ -12,7 +12,7 @@ kern.setTextEncoding(None)
 kern.bootstrap(learnFiles="mybot-basic.xml")
 
 # Welcome user
-print("Welcome to this chat bot. Please feel free to ask questions from me!")
+print("Welcome to this chat bot. Please feel free to ask questions from me!\nI am specifically designed for volleyball questions.\nIf you want to know more about a specific match type in 'Volleyball Match' followed by an ID (e.g. 11500).")
 
 # Main loop
 while True:
@@ -40,14 +40,59 @@ while True:
                 print(wSummary)
             except:
                 print("Sorry, I do not know that. Be more specific!")
-        elif cmd ==2:
-            match_id = params[1]
-            v = fivbvis.Volleyball()
-            # Get the data, requesting specific fields to make the response clear
-            match_data = v.getVolleyMatch(no=match_id, fields="City TeamNameA TeamNameB DateLocal ScoreA ScoreB")
-            # Parse match_data (you'll need to handle the XML/JSON) and print a nice response
-            print(
-                f"Match {match_id} was played in {match_data.City} on {match_data.DateLocal}. The score was {match_data.TeamNameA} {match_data.ScoreA} - {match_data.ScoreB} {match_data.TeamNameB}.")
+        elif cmd == 2:
+                match_id = params[1]
+                try:
+                    v = fivbvis.Volleyball()
+                    # Get the data, requesting specific fields
+                    match_data = v.getVolleyMatch(no=match_id,
+                                                  fields="City TeamNameA TeamNameB CountryName DateLocal MatchPointsA MatchPointsB")
+
+                    root = ET.fromstring(match_data)
+
+                    # Extract data from XML attributes
+                    city = root.get('City', 'Unknown')
+                    team_a = root.get('TeamNameA', 'Unknown')
+                    team_b = root.get('TeamNameB', 'Unknown')
+                    country = root.get('CountryName', 'Unknown')
+                    date = root.get('DateLocal', 'Unknown')
+                    points_a = root.get('MatchPointsA', 'Unknown')
+                    points_b = root.get('MatchPointsB', 'Unknown')
+
+                    # Check if this is a valid match (has at least a match number)
+                    match_no = root.get('No', '')
+
+                    # If the match number doesn't match what we requested or is missing, it's invalid
+                    if not match_no or match_no != match_id:
+                        print(f"No match found with ID {match_id}")
+
+                    # Check if all essential fields are 'Unknown' (likely invalid match)
+                    elif city == 'Unknown' and team_a == 'Unknown' and team_b == 'Unknown':
+                        print(f"No match found with ID {match_id}")
+
+                    # We have at least some real data
+                    else:
+                        # Check if points exist (not Unknown and not empty)
+                        if points_a not in ['Unknown', ''] and points_b not in ['Unknown', '']:
+                            print(
+                                f"Match {match_id} was played in {city}, {country} on {date} between {team_a} and {team_b} and finished {points_a} - {points_b}.")
+
+                        # We have match info but no points
+                        elif team_a not in ['Unknown', ''] and team_b not in ['Unknown', '']:
+                            print(
+                                f"Match {match_id} was played in {city}, {country} on {date} between {team_a} and {team_b}. No points are available for this match.")
+
+                        # Very limited info
+                        else:
+                            print(
+                                f"Match {match_id} was played in {city}, {country} on {date}. Limited information is available for this match.")
+
+                except ET.ParseError as e:
+                    print(f"Sorry, I couldn't parse the match data. Error: {e}")
+                except Exception as e:
+                    print(f"Sorry, I couldn't find that volleyball match. Error: {e}")
+        elif cmd == 3:
+            player_name = params[1]
         elif cmd == 99:
             print("I did not get that, please try again.")
     else:
